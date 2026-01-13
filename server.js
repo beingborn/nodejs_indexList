56744d80d11f344159a72e5b527764fae8b46311
beingborn <dolmin0708@naver.com>
2026-01-14 00:37:51 +0900

    (20260113)  업데이트 status 작업 진행중

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const cors = require('cors'); 
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
+
+const fs = require('fs');
+const fileName = './pageData.json';
+const file = require(fileName);
 
 // 모든 출처 허용 (Develop)
 app.use(cors());
+
+// Json Parse
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('안녕하세요.')
@@ -14,11 +23,27 @@ app.listen(port, () => {
     console.log(`서버가 ${port}에서 실행중입니다.`)
 })
 
-// Post id를 받으면 해당 JSON을 읽어와 값을 수정 시킨다.
-// 그리고 저장
-
+// 완료 여부 체크
 app.post("/update-status", function(req, res) {
-    console.log(req.body);
+    var data = req.body;
+    var targetPage = file.find((page) => page.id == data.id);
 
-    return res.send({success: true});
+    console.log(targetPage)
+
+    if (targetPage) {
+        targetPage.status = data.status;
+
+        fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("파일 저장 중 오류 발생");
+        }
+            console.log('Updated ID:', data.id, 'to Status:', data.status);
+            return res.send("상태가 변경되었습니다.");
+        });
+    } else {
+        return res.status(404).send("해당 페이지를 찾을 수 없습니다.");
+    }
+    
+    return res.send("제목 변경");
 })
